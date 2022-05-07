99f9c9a42c542d004f084cab9d6a199c77311dfa
Micah <micahnyoung95@gmail.com>
2022-05-07 15:20:30 -0500

    first commit

---
+++ src/main/java/org/launchcode/testProject/controllers/HelloController.java
@@ -0,0 +1,52 @@
+package org.launchcode.testProject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping(value = "hello")
+public class HelloController {
+
+    //Handles request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("goodbye")
+
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    //Handles request of the form/hello?name=LaunchCode
+
+    @RequestMapping (method = {RequestMethod.GET,RequestMethod.POST})
+
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    //Handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
+
+    public String helloWithPathName(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+
+    @GetMapping("form")
+
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" + //submit a request to /hello
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet Me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+}
